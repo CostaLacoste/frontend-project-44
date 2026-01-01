741345ecb5cd04ba9be521dd8884ab8d557ceb4e
bogdanm <cwdw@icloud.com>
2026-01-01 22:08:42 +0400

    Сделал движок

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,35 +1,15 @@
 #!/usr/bin/env node
 
-import { question } from "readline-sync";
 import { isEven, genNum } from "../src/help.js";
+import runGame from "../src/runGame.js";
+
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const genQuestion = () => {
-    const num = genNum();
-    const answer = isEven(num) ? yes : no;
+  const num = genNum();
+  const answer = isEven(num) ? 'yes' : 'no';
 
-    return {
-        question: String(num),
-        answer,
-    };
+  return [String(num), answer];
 };
 
-const roundsCount = 3;
-
-for (let i = 0; i < roundsCount; i += 1) {
-  const { question, correctAnswer } = genQuestion();
-
-  console.log(`Question: ${question}`);
-  const userAnswer = readlineSync.question('Your answer: ').trim().toLowerCase();
-
-  if (userAnswer !== correctAnswer) {
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-    console.log("Let's try again");
-    process.exit(0);
-  }
-
-  console.log('Correct!');
-};
-
-const description = `Answer "yes" if the number is even, otherwise answer "no".`;
-console.log(description);
-console.log(genQuestion());
+runGame(description, genQuestion);
