3f9f2e2351239ad682d62b213dde2046d720516a
bogdanm <cwdw@icloud.com>
2026-02-11 13:16:12 +0300

    Elsint fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,18 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-]);
+export default [
+  js.configs.recommended,
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/eol-last': ['error', 'always'],
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+    },
+  },
+]
